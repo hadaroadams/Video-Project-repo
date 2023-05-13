bdb530971819fdff674245b077fb1f6669962b4d
hadaroadams <hadaroadams1234@gmail.com>
2023-05-13 18:08:32 +0000

    loading screan design

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-console.log('hello world')
+let gifImage = document.querySelector(".gifImageDiv")
 let myVideo= document.querySelector('video')
 let myButton=document.querySelector('button')
 let design = document.querySelector('.switch')
+
+//loading screan
+window.addEventListener("DOMContentLoaded",()=>{
+    setTimeout(()=>{
+        gifImage.style.display="none"
+    },3000)
+})
+
 
 myButton.addEventListener('click',()=>{
     let state = design.classList.toggle("left")
